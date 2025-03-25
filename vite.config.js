5fed375409bf7ba5e2d254a05c48b7c7e69c5a1f
Dheeraj Sharma <greatnerve@gmail.com>
2025-03-25 12:07:39 +0530

    vite reroute

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path';
-import tailwindcss from '@tailwindcss/vite';
-
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target:
+          "http://aa09e3ae27a144cd0aaf0fbce7f0ab1c-bcccf20eeb9b21bd.elb.ap-south-1.amazonaws.com",
+        changeOrigin: true,
+        secure: false, // Since backend doesn't have SSL
+        rewrite: (path) => path.replace(/^\/api/, ""), // Ensure proper request forwarding
+      },
     },
   },
 });
-
-
