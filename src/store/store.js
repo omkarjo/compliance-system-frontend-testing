208655bb551708461d0b24b462d90e347cd3894b
Dheeraj Sharma <greatnerve@gmail.com>
2025-06-16 18:46:12 +0530

    chore: update dependencies

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { thunk } from "redux-thunk"; 
 import userReducer from "./slices/userSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(), 
 });
 
 export default store;
